f15a288e9fd82c1223a80e6b2b8606e412503860
Michael John G. Margate <101804266+Miketzy@users.noreply.github.com>
2025-09-28 21:44:12 +0800

    updated

--- admin-contributor/src/App.jsx
+++ admin-contributor/src/App.jsx
@@ -2,11 +2,11 @@ function App() {
   return (
     <>
       <div className="flex">
-        <div className="bg-gray-100">
+        <div className=" px-10 bg-gray-100">
           <h1>SIDEBAR</h1>
         </div>
         <div className="flex flex-col">
-          <div className="bg-gray-100">
+          <div className="px-10 bg-gray-100">
             <h1>NAVBAR</h1>
           </div>
           <div className="bg-gray-100">
