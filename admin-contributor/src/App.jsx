2c3dbe0e3d06bdca082a7cdfd5bc42be256c6160
Michael John G. Margate <101804266+Miketzy@users.noreply.github.com>
2025-09-28 21:51:07 +0800

    updated

--- admin-contributor/src/App.jsx
+++ admin-contributor/src/App.jsx
@@ -1,12 +1,12 @@
 function App() {
   return (
     <>
-      <div className="flex">
-        <div className=" px-10 py-10 bg-gray-100 h-full">
+      <div className="flexmin-h-screen">
+        <div className=" px-10 py-10 bg-gray-100 h-full w-60 ">
           <h1>SIDEBAR</h1>
         </div>
         <div className="flex flex-col gap-10">
-          <div className="px-10 py-10 bg-gray-100 w-full">
+          <div className="px-10 py-10 bg-gray-100 w-full h-60">
             <h1>NAVBAR</h1>
           </div>
           <div className="bg-gray-100">
