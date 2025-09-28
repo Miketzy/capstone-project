ef02ef7a988203dd92032597fb684cd01e8c77ba
Michael John G. Margate <101804266+Miketzy@users.noreply.github.com>
2025-09-28 22:04:29 +0800

    updated

--- admin-contributor/src/App.jsx
+++ admin-contributor/src/App.jsx
@@ -9,7 +9,7 @@ function App() {
           <div className="px-10 py-10 bg-gray-100 w-full">
             <h1>NAVBAR</h1>
           </div>
-          <div className="grid grid-cols-3 w-full gap-5">
+          <div className="grid grid-cols-3 w-full gap-5 px-10 py-5">
             <h1 className="bg-gray-100 ">NAVBAR</h1>
             <h1 className="bg-gray-100 ">NAVBAR</h1>
             <h1 className="bg-gray-100 ">NAVBAR</h1>
