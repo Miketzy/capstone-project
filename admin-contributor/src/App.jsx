3fcaa46ededfe0ca658c114b0cd75a267873e9ec
Michael John G. Margate <101804266+Miketzy@users.noreply.github.com>
2025-09-29 18:03:29 +0800

    admin-contributor app updated

--- admin-contributor/src/App.jsx
+++ admin-contributor/src/App.jsx
@@ -1,22 +1,7 @@
 function App() {
   return (
     <>
-      <div className="flex min-h-screen">
-        <div className=" px-10 py-10 bg-gray-100 ">
-          <h1>SIDEBAR</h1>
-        </div>
-        <div className="flex flex-col gap-10 flex-1">
-          <div className="px-10 py-10 bg-gray-100 w-full">
-            <h1>NAVBAR</h1>
-          </div>
-          <div className="grid grid-cols-2 lg:grid-cols-3 w-full gap-5 px-10 py-5">
-            <h1 className="bg-gray-100 ">NAVBAR</h1>
-            <h1 className="bg-gray-100 ">NAVBAR</h1>
-            <h1 className="bg-gray-100 ">NAVBAR</h1>
-            <h1 className="bg-gray-100 ">NAVBAR</h1>
-          </div>
-        </div>
-      </div>
+      <div className="layout"></div>
     </>
   );
 }
