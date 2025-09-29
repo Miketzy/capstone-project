6cfe7c978b7ef230e7e22f5dfa1cc91b48641592
Michael John G. Margate <101804266+Miketzy@users.noreply.github.com>
2025-09-29 20:34:37 +0800

    admin updated

--- admin-contributor/src/admin-components/layout/Sidebar.jsx
+++ admin-contributor/src/admin-components/layout/Sidebar.jsx
@@ -1,9 +1,18 @@
 import React from "react";
+import { Zap } from "lucide-react";
 
 function Sidebar() {
   return (
     <>
-      <div>Sidebar</div>
+      <div className="sidebar">
+        <div className="sidebar-logo">
+          <div className="logo-layout">
+            <div className="logo">
+              <Zap className="Zap-layout" />
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   );
 }
