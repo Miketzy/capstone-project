857550503dfaa04bbb980d82514924a0c20f7ac0
Michael John G. Margate <101804266+Miketzy@users.noreply.github.com>
2025-10-03 10:57:01 +0800

    admin header updated

--- admin-contributor/src/admin-components/layout/Header.jsx
+++ admin-contributor/src/admin-components/layout/Header.jsx
@@ -1,10 +1,18 @@
+import { Menu } from "lucide-react";
 import React from "react";
 
 function Header() {
   return (
     <>
-      <div className="bg-white/-80 backdrop-blur-xl border-b border-slate-200/50 px-6 py-4">
-        Header
+      <div className="bg-white/80 backdrop-blur-xl border-b border-slate-200/50 px-6 py-4">
+        <div className="flex items-center justify-between">
+          {/*Left Section */}
+          <div className="flex items-center space-x-4">
+            <button className="p-2 rounded-lg text-slate-600 hover:ng-slate-100 transition-colors">
+              <Menu className="w-5 h-5 " />
+            </button>
+          </div>
+        </div>
       </div>
     </>
   );
