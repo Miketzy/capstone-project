6700613b1c9a6356a1a12a5b7d17ef0cdeb6a3c6
Michael John G. Margate <101804266+Miketzy@users.noreply.github.com>
2025-10-03 16:19:59 +0800

    updated

--- admin-contributor/src/admin-components/layout/Header.jsx
+++ admin-contributor/src/admin-components/layout/Header.jsx
@@ -32,7 +32,7 @@ function Header() {
                 placeholder="Search Anything"
                 className="w-full pl-10 pr-4 py-2.5 border border-slate-200 rounded-xl 
              bg-gradient-to-r from-[#379564] to-[#22573b] 
-             text-white placeholder-white 
+             text-white placeholder-slate-200
              focus:outline-none focus:ring-2 focus:ring-blue-500 
              focus:border-transparent transition-all"
               />
