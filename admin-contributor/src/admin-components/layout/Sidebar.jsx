2e167b666d3313868abbf5e3ce93fd3dbfe898e9
Michael John G. Margate <101804266+Miketzy@users.noreply.github.com>
2025-10-03 09:58:48 +0800

    updated

--- admin-contributor/src/admin-components/layout/Sidebar.jsx
+++ admin-contributor/src/admin-components/layout/Sidebar.jsx
@@ -18,6 +18,22 @@ function Sidebar() {
             </div>
           </div>
         </div>
+
+        {/*Navigation display a dynamic menu*/}
+        <nav className="navigation-container"></nav>
+
+        {/*user profile*/}
+        <div className="userprofile-container">
+          <div className="user-profile">
+            <img src="" alt="user" className="user-image" />
+            <div className="flex-1 min-2-0">
+              <div className="flex-1 min-2-0">
+                <p className="user-name">Michael John G. Margate</p>
+                <p className="administration">Administration</p>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </>
   );
