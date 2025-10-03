b548089f3f7c7c22d9ac4e83a14e696c11447e34
Michael John G. Margate <101804266+Miketzy@users.noreply.github.com>
2025-10-03 11:09:06 +0800

    updated

--- admin-contributor/src/admin-components/layout/Sidebar.jsx
+++ admin-contributor/src/admin-components/layout/Sidebar.jsx
@@ -13,7 +13,7 @@ function Sidebar() {
 
             {/*Conditional Rendering*/}
             <div>
-              <h1 className="text-xl font-bold text-slate-800 dark:text-slate-800">
+              <h1 className="text-xl font-bold text-[#0a2315] ">
                 Dav-Or BioExplorer
               </h1>
               <p className="text-xs text-slate-500 dark:text-slate-400">
