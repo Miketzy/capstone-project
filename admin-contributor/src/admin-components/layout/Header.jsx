2ec65d4775da7d953c42de913d8758311f6fc1f4
Michael John G. Margate <101804266+Miketzy@users.noreply.github.com>
2025-10-03 14:56:56 +0800

    updated

--- admin-contributor/src/admin-components/layout/Header.jsx
+++ admin-contributor/src/admin-components/layout/Header.jsx
@@ -8,7 +8,7 @@ function Header() {
         <div className="flex items-center justify-between">
           {/*Left Section */}
           <div className="flex items-center space-x-4">
-            <button className="p-2 rounded-lg text-white hover:ng-slate-100 transition-colors cursor-pointer">
+            <button className="p-2 rounded-lg text-black hover:ng-slate-100 transition-colors cursor-pointer">
               <Menu className="w-5 h-5 " />
             </button>
 
