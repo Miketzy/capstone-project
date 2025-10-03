77c0cee5393beb7ecf242cb257b5b46aa4fd8450
Michael John G. Margate <101804266+Miketzy@users.noreply.github.com>
2025-10-03 16:13:49 +0800

    updated

--- admin-contributor/src/admin-components/layout/Header.jsx
+++ admin-contributor/src/admin-components/layout/Header.jsx
@@ -27,15 +27,15 @@ function Header() {
           <div className="flex-1 max-w-md mx-8">
             <div className="relative">
               <Search className="w-4 h-4 absolute left-3 top-1/2 transform -translate-y-1/2 bg-gradient-to-r from-[#379564] to-[#22573b] bg-clip-text text-transparent" />
-
               <input
                 type="text"
                 placeholder="Search Anything"
-                className="w-full pl-10 pr-4 py-2.5 border border-slate-200 text-white rounded-xl 
-                 bg-slate-800 placeholder-white focus:outline-none focus:ring-2 focus:ring-blue-500 
-                 focus:border-transparent transition-all"
+                className="w-full pl-10 pr-4 py-2.5 border border-slate-200 rounded-xl 
+             bg-gradient-to-r from-[#379564] to-[#22573b] 
+             text-white placeholder-white 
+             focus:outline-none focus:ring-2 focus:ring-blue-500 
+             focus:border-transparent transition-all"
               />
-
               <button
                 className="absolute right-2 top-1/2 transform -translate-y-1/2 p-1.5
                  text-slate-400 hover:text-slate-600"
