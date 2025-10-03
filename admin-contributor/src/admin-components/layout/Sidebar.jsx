031452f10a13381aef59e23702a20a2d7a190585
Michael John G. Margate <101804266+Miketzy@users.noreply.github.com>
2025-10-03 11:59:28 +0800

    updated

--- admin-contributor/src/admin-components/layout/Sidebar.jsx
+++ admin-contributor/src/admin-components/layout/Sidebar.jsx
@@ -13,7 +13,7 @@ function Sidebar() {
 
             {/*Conditional Rendering*/}
             <div>
-              <h1 className="text-xl font-bold text-white ">
+              <h1 className="text-xl font-bold text-sm font-medium truncate bg-gradient-to-r from-[#379564] to-[#22573b]">
                 Dav-Or BioExplorer
               </h1>
               <p className="text-xs text-white">Admin Panel</p>
@@ -34,7 +34,7 @@ function Sidebar() {
             />
             <div className="flex-1 min-2-0">
               <div className="flex-1 min-2-0">
-                <p className="text-sm font-medium truncate bg-gradient-to-r from-[#379564] to-[#22573b] bg-clip-text text-transparent">
+                <p className="text-sm font-medium text-slate-800 dark:text-white truncate">
                   Michael John G. Margate
                 </p>
                 <p className="text-xs text-white truncate">Administrator</p>
