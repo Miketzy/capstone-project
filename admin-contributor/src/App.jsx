4dfc87ac7d74890ce8a30b43f8074ab8b5d3fb42
Michael John G. Margate <101804266+Miketzy@users.noreply.github.com>
2025-10-04 18:53:52 +0800

    updated

--- admin-contributor/src/App.jsx
+++ admin-contributor/src/App.jsx
@@ -7,7 +7,7 @@ function App() {
       <div className="container-layout">
         <div className="layout">
           <Sidebar />
-          <div className="flex-1 flex flex-col overflow-hidden">
+          <div className="flex-1 flex flex-col ">
             <Header />
             <footer className="fixed bottom-0 left-0 w-full bg-black text-white text-center py-3 block md:hidden">
               © 2025 Dav-Or BioExplorer. All rights reserved.
