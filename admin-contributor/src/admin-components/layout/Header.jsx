29b6db485723be2174d9dcf7ce3eeca53e2bef7d
Michael John G. Margate <101804266+Miketzy@users.noreply.github.com>
2025-10-04 17:57:47 +0800

    updated

--- admin-contributor/src/admin-components/layout/Header.jsx
+++ admin-contributor/src/admin-components/layout/Header.jsx
@@ -1,4 +1,13 @@
-import { Bell, Filter, Inbox, Menu, Plus, Search, Sun } from "lucide-react";
+import {
+  Bell,
+  Filter,
+  Inbox,
+  Menu,
+  Plus,
+  Search,
+  Sun,
+  Zap,
+} from "lucide-react";
 import React from "react";
 
 function Header() {
@@ -10,7 +19,12 @@ function Header() {
 
           <div className="flex items-center space-x-4 block md:hidden">
             <div className="p-2 rounded-lg">
-              <Sun className="w-5 h-5" />
+              <div
+                className="w-10 h-10 bg-gradient-to-r from-[#379564] to-[#22573b] rounded-xl 
+    flex items-center justify-center shadow-lg"
+              >
+                <Zap className="w-6 h-6 text-white" />
+              </div>
             </div>
           </div>
 
