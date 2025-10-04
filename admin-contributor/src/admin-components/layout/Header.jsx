8b14e0eec8b84160677b165cb40879d95d35779f
Michael John G. Margate <101804266+Miketzy@users.noreply.github.com>
2025-10-04 19:06:33 +0800

    updated

--- admin-contributor/src/admin-components/layout/Header.jsx
+++ admin-contributor/src/admin-components/layout/Header.jsx
@@ -15,7 +15,7 @@ import React from "react";
 function Header() {
   return (
     <>
-      <div className="sticky top-0 z-40 bg-white backdrop-blur-xl border-b border-slate-200/50 px-6 py-4">
+      <div className="fixed top-0 left-0 right-0 z-40 bg-white backdrop-blur-xl border-b border-slate-200/50 px-6 py-4">
         <div className="flex items-center justify-between">
           {/*Left Section */}
 
