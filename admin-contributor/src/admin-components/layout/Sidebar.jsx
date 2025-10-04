2f0bade2c158588d009eb599a10d02b699b5b26a
Michael John G. Margate <101804266+Miketzy@users.noreply.github.com>
2025-10-04 19:09:25 +0800

    updated

--- admin-contributor/src/admin-components/layout/Sidebar.jsx
+++ admin-contributor/src/admin-components/layout/Sidebar.jsx
@@ -4,7 +4,7 @@ import { Zap } from "lucide-react";
 function Sidebar() {
   return (
     <>
-      <div className="sidebar hidden md:flex  fixed top-0 left-0 h-full z-40">
+      <div className="sidebar hidden md:flex sticky top-0 z-40">
         <div className="sidebar-logo">
           <div className="logo-layout">
             <div className="logo">
