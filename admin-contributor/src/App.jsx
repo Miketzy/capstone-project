74f29ab5c49b442701b45a765f0bc9297e8e05bf
Michael John G. Margate <101804266+Miketzy@users.noreply.github.com>
2025-10-05 08:50:57 +0800

    updated

--- admin-contributor/src/App.jsx
+++ admin-contributor/src/App.jsx
@@ -14,7 +14,7 @@ function App() {
 
         <div className="flex flex-col">
           <div className="px-6 py-4">
-            <footer className="w-full bg-black text-white text-center py-3 ">
+            <footer className="fixed bottom-0 left-0 w-full bg-black text-white text-center py-3 ">
               © 2025 Dav-Or BioExplorer. All rights reserved.
             </footer>
           </div>
