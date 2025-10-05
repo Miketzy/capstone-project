e6b3525c29c5cf2762c40d119ec41b2f9815f1e2
Michael John G. Margate <101804266+Miketzy@users.noreply.github.com>
2025-10-05 08:38:14 +0800

    updated

--- admin-contributor/src/admin-components/layout/Header.jsx
+++ admin-contributor/src/admin-components/layout/Header.jsx
@@ -84,12 +84,6 @@ function Header() {
               </span>
             </button>
 
-            <button className="relative p-2.5 rounded-xl text-slate-600 hover:bg-slate-100 transition-colors">
-              <Bell className="w-5 h-5" />
-              <span className="absolute -top-1 w-5 h-5 bg-red-500 text-white text-xs rounded-full flex items-center justify-center">
-                3
-              </span>
-            </button>
             {/*Setting */}
             <button className="p-2.5 rounded-xl text-slate-600 hover:bg-slate-100 transition-color">
               <Settings className="w-5 h-5" />
