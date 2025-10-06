07237da4b99387206d3f1e838b8e29c2a225c098
Michael John G. Margate <101804266+Miketzy@users.noreply.github.com>
2025-10-06 19:18:08 +0800

    updated

--- admin-contributor/src/App.jsx
+++ admin-contributor/src/App.jsx
@@ -1,30 +1,8 @@
-import {
-  LayoutDashboard,
-  List,
-  Leaf,
-  PlusCircle,
-  MessageSquare,
-  BarChart3,
-  Users,
-  Image,
-} from "lucide-react";
 import Header from "./admin-components/layout/Header";
 import Sidebar from "./admin-components/layout/Sidebar";
-import { useState } from "react";
 import Navbar from "./admin-components/icon-navbar/Navbar";
 
 function App() {
-  const [active, setActive] = useState("dashboard");
-
-  const navItems = [
-    { id: "dashboard", icon: LayoutDashboard },
-    { id: "add species", icon: Leaf },
-    { id: "list", icon: List },
-    { id: "Analytic", icon: BarChart3 },
-    { id: "createQuestion", icon: PlusCircle },
-    { id: "contributorRequest", icon: MessageSquare },
-    { id: "gallery", icon: Image },
-  ];
   return (
     <>
       <div className="container-layout">
