ff450a84842f4da8578a062df4e8e30f06f73cc6
Michael John G. Margate <101804266+Miketzy@users.noreply.github.com>
2025-10-06 19:43:01 +0800

    updated

--- admin-contributor/src/admin-components/layout/Sidebar.jsx
+++ admin-contributor/src/admin-components/layout/Sidebar.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Zap } from "lucide-react";
+import UserProfile from "./UserProfile";
 
 function Sidebar() {
   return (
@@ -28,21 +29,7 @@ function Sidebar() {
 
         {/*user profile*/}
         <div className="p-4 border-t border-slate-200/50">
-          <div className="flex items-center space-x-3 p-3 rounded-xl bg-gradient-to-r from-[#379564] to-[#22573b]">
-            <img
-              src=""
-              alt="user"
-              className="w-10 h-10 rounded-full ring-2 ring-blue-500"
-            />
-            <div className="flex-1 min-2-0">
-              <div className="flex-1 min-2-0">
-                <p className="text-sm font-medium text-slate-800 dark:text-white truncate">
-                  Michael John G. Margate
-                </p>
-                <p className="text-xs text-slate-200 truncate">Administrator</p>
-              </div>
-            </div>
-          </div>
+          <UserProfile />
         </div>
       </div>
     </>
---
+++ admin-contributor/src/admin-components/layout/UserProfile.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+function UserProfile() {
+  return (
+    <>
+      <div className="flex items-center space-x-3 p-3 rounded-xl bg-gradient-to-r from-[#379564] to-[#22573b]">
+        <img
+          src=""
+          alt="user"
+          className="w-10 h-10 rounded-full ring-2 ring-blue-500"
+        />
+        <div className="flex-1 min-2-0">
+          <div className="flex-1 min-2-0">
+            <p className="text-sm font-medium text-slate-800 dark:text-white truncate">
+              Michael John G. Margate
+            </p>
+            <p className="text-xs text-slate-200 truncate">Administrator</p>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default UserProfile;
