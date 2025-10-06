e0d1638c72521bc395bfe1d8eb8d28f901f0ab08
Michael John G. Margate <101804266+Miketzy@users.noreply.github.com>
2025-10-06 23:07:39 +0800

    updated

--- admin-contributor/src/admin-components/layout/HeaderCenterSection.jsx
+++ admin-contributor/src/admin-components/layout/HeaderCenterSection.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Filter } from "lucide-react";
+import { Filter, Search } from "lucide-react";
 
 function HeaderCenterSection() {
   return (
--- admin-contributor/src/admin-components/layout/HeaderLeftSection.jsx
+++ admin-contributor/src/admin-components/layout/HeaderLeftSection.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-import { Zap } from "lucide-react";
+import { Zap, Menu } from "lucide-react";
 
 function HeaderLeftSection() {
   return (
