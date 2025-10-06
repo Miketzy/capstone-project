707383808caf95bb35819836677ffd91017e3e09
Michael John G. Margate <101804266+Miketzy@users.noreply.github.com>
2025-10-06 19:47:19 +0800

    updated

---
+++ admin-contributor/src/admin-components/layout/Logo.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+function Logo() {
+  return (
+    <>
+      <div className="logo-layout">
+        <div className="logo">
+          <Zap className="Zap-layout" />
+        </div>
+
+        {/*Conditional Rendering*/}
+        <div>
+          <h1 className="text-xl font-bold bg-gradient-to-r from-[#379564] to-[#22573b] bg-clip-text ">
+            <strong>Dav-Or BioExplorer</strong>
+          </h1>
+          <p className="text-xs bg-gradient-to-r from-[#379564] to-[#22573b] bg-clip-text ">
+            Admin Panel
+          </p>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Logo;
