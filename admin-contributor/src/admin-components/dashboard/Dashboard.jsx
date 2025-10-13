f886387f9834444924c36c83d2cfb2dae683dc85
Michael John G. Margate <101804266+Miketzy@users.noreply.github.com>
2025-10-13 09:46:19 +0800

    updated

--- admin-contributor/src/admin-components/dashboard/Dashboard.jsx
+++ admin-contributor/src/admin-components/dashboard/Dashboard.jsx
@@ -6,10 +6,10 @@ function Dashboard() {
       <div>
         <div className="flex justify-center gap-4">
           <button className="bg-white text-black px-5 py-2 rounded-xl hover:bg-green-70 transition">
-            Add Species
+            Vertebrates
           </button>
           <button className="bg-white text-black px-5 py-2 rounded-xl hover:bg-green-70 transition">
-            Add Species
+            Invertebrates
           </button>
         </div>
       </div>
---
+++ admin-contributor/src/admin-components/dashboard/vertebratesCard/VertebratesCard.jsx
@@ -0,0 +1,46 @@
+import { Bird, Fish, PawPrint, Turtle, Frog } from "lucide-react";
+import React from "react";
+
+function VertebratesCard() {
+  const vertebratesItem = [
+    {
+      title: "Mammals",
+      icon: PawPrint,
+      count: "45",
+    },
+    {
+      title: "Fish",
+      icon: Fish,
+      count: "45",
+    },
+    {
+      title: "Birds",
+      icon: Bird,
+      count: "45",
+    },
+    {
+      title: "Reptiles",
+      icon: Turtle,
+      count: "45",
+    },
+    {
+      title: "Amphibians",
+      icon: Frog,
+      count: "45",
+    },
+  ];
+  return (
+    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
+      {vertebratesItem.map((item, index) => (
+        <div key={index} className="bg-white rounded-2xl shadow-md p-5 ">
+          <div className="flex">
+            <item.icon className="w-6 h-6 text-green-600" />
+            <h2 className="text-xl font-semibold text-black">{item.title}</h2>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default VertebratesCard;
