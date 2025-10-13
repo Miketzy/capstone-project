83b322fd46c1e8332f1e3f0970ec858726aa7c66
Michael John G. Margate <101804266+Miketzy@users.noreply.github.com>
2025-10-13 18:50:35 +0800

    updated

--- admin-contributor/src/admin-components/add-species/AddSpecies.jsx
+++ admin-contributor/src/admin-components/add-species/AddSpecies.jsx
@@ -24,6 +24,8 @@ function AddSpecies() {
                 placeholder="Enter your name"
                 className="w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-green-500"
               />
+            </div>
+            <div className="mb-4 text-left w">
               <label className="block text-gray-700 mb-2">Full Name</label>
               <input
                 type="text"
