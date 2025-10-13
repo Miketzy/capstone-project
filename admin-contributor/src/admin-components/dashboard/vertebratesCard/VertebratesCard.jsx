e09a4aa9b4ff37621c84103ba4654a62375b88da
Michael John G. Margate <101804266+Miketzy@users.noreply.github.com>
2025-10-13 12:41:27 +0800

    updated

--- admin-contributor/src/admin-components/dashboard/vertebratesCard/VertebratesCard.jsx
+++ admin-contributor/src/admin-components/dashboard/vertebratesCard/VertebratesCard.jsx
@@ -38,7 +38,9 @@ function VertebratesCard() {
             <item.icon className="w-6 h-6 text-green-600" />
             <h2 className="text-xl font-semibold text-black">{item.title}</h2>
           </div>
-          <div className="font-medium text-black">{item.count}</div>
+          <div className="mt-4">
+            <span className="font-medium text-black">{item.count}</span>
+          </div>
         </div>
       ))}
     </div>
