147206ed68166e9b1f40d338350ba24dcdfe691f
Michael John G. Margate <101804266+Miketzy@users.noreply.github.com>
2025-12-24 11:35:00 +0800

    add species

--- admin-contributor/src/admin-components/add-species/AddSpecies.jsx
+++ admin-contributor/src/admin-components/add-species/AddSpecies.jsx
@@ -25,40 +25,42 @@ function AddSpecies() {
               className="block w-full text-sm text-gray-700 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 focus:outline-none focus:ring-2 focus:ring-green-500"
             />
           </div>
+          <div className="flex">
+            <div className="flex flex-col md:flex-row gap-10">
+              <div className="mb-4 text-left w">
+                <label className="block text-gray-700 mb-2">Full Name</label>
+                <input
+                  type="text"
+                  placeholder="Enter your name"
+                  className="w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-green-500"
+                />
+              </div>
+              <div className="mb-4 text-left w">
+                <label className="block text-gray-700 mb-2">Full Name</label>
+                <input
+                  type="text"
+                  placeholder="Enter your name"
+                  className="w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-green-500"
+                />
+              </div>
+            </div>
+            <div className="mb-6 text-left">
+              <label className="block text-gray-700 mb-2">Email</label>
+              <input
+                type="email"
+                placeholder="Enter your email"
+                className="w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-green-500"
+              />
+            </div>
+            {/* Submit Button */}
+            <button
+              type="submit"
+              className="w-full bg-gradient-to-r from-[#379564] to-[#22573b] text-white py-2 rounded-lg hover:opacity-90 transition"
+            >
+              Submit
+            </button>{" "}
+          </div>
           {/* Other Input Fields */}
-          <div className="flex flex-col md:flex-row gap-10">
-            <div className="mb-4 text-left w">
-              <label className="block text-gray-700 mb-2">Full Name</label>
-              <input
-                type="text"
-                placeholder="Enter your name"
-                className="w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-green-500"
-              />
-            </div>
-            <div className="mb-4 text-left w">
-              <label className="block text-gray-700 mb-2">Full Name</label>
-              <input
-                type="text"
-                placeholder="Enter your name"
-                className="w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-green-500"
-              />
-            </div>
-          </div>
-          <div className="mb-6 text-left">
-            <label className="block text-gray-700 mb-2">Email</label>
-            <input
-              type="email"
-              placeholder="Enter your email"
-              className="w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-green-500"
-            />
-          </div>
-          {/* Submit Button */}
-          <button
-            type="submit"
-            className="w-full bg-gradient-to-r from-[#379564] to-[#22573b] text-white py-2 rounded-lg hover:opacity-90 transition"
-          >
-            Submit
-          </button>
         </form>
       </div>
     </>
