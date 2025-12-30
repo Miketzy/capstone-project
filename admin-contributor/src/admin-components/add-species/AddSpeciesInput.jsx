63f85023ee54a6076a601d2f0fb152a03ce21e1c
Michael John G. Margate <101804266+Miketzy@users.noreply.github.com>
2025-12-30 11:10:20 +0800

    addspeciesinput-select

--- admin-contributor/src/admin-components/add-species/AddSpeciesInput.jsx
+++ admin-contributor/src/admin-components/add-species/AddSpeciesInput.jsx
@@ -35,7 +35,13 @@ function AddSpeciesInput() {
         <div className="mb-4 text-left w-full md:w-1/2">
           <label className="block text-gray-700 mb-2">Classification</label>
           <select
-            className="w-full px-4 py-2 border rounded-lg focus:ring-2 focus:ring-green-500 cursor-pointer"
+            className="
+      w-full px-4 py-2 border rounded-lg cursor-pointer
+      transition-all duration-300
+      hover:bg-gradient-to-r hover:from-[#379564] hover:to-[#22573b]
+      hover:text-white
+      focus:ring-2 focus:ring-green-500
+    "
             defaultValue=""
           >
             <option value="" disabled>
