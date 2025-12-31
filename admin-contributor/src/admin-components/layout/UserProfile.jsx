d69c2e966738efbd81abecec3a66170fbac19957
Michael John G. Margate <101804266+Miketzy@users.noreply.github.com>
2025-12-31 11:44:23 +0800

    updated

--- admin-contributor/src/admin-components/layout/UserProfile.jsx
+++ admin-contributor/src/admin-components/layout/UserProfile.jsx
@@ -4,12 +4,12 @@ function UserProfile() {
   return (
     <div className="flex items-center space-x-3 p-3 rounded-xl bg-gradient-to-r from-[#379564] to-[#22573b]">
       <img
-        src="/images/sd" // original image
+        src="/images/download.jpg" // original image
         alt="user"
         className="w-10 h-10 rounded-full ring-2 ring-blue-500"
         onError={(e) => {
           e.target.onerror = null; // prevent infinite loop
-          e.target.src = "/images/download.jpg"; // default image
+          e.target.src = "/images/unknown.jpg"; // default image
         }}
       />
       <div className="flex-1 min-w-0 hidden lg:inline">
