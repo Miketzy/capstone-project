27ca4f2283e4a5ae67f4e198aa95f77ccb70556b
Michael John G. Margate <101804266+Miketzy@users.noreply.github.com>
2026-01-04 10:42:21 +0800

    updated

--- admin-contributor/src/App.jsx
+++ admin-contributor/src/App.jsx
@@ -35,7 +35,7 @@ function App() {
           </div>
         </div>
 
-        <div className="block md:hidden">
+        <div className="block md:hidden mt-2 ">
           <Navbar currentPage={currentPage} onPageChange={setCurrentPage} />
         </div>
       </div>
