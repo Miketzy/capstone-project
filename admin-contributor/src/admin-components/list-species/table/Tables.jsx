2463e349f195e5ac6fc2b2cbcf12e7ebbef24db7
Michael John G. Margate <101804266+Miketzy@users.noreply.github.com>
2026-01-04 12:21:30 +0800

    updated

--- admin-contributor/src/admin-components/list-species/table/Tables.jsx
+++ admin-contributor/src/admin-components/list-species/table/Tables.jsx
@@ -9,34 +9,22 @@ function Tables({ items, currentPage, itemsPerPage }) {
           <tr>
             <th className="px-4 py-2 text-left">No.</th>
             <th className="px-4 py-2 text-left">Name</th>
-            <th className="px-4 py-2 text-left hidden sm:table-cell">
-              Classification
-            </th>
-            <th className="px-4 py-2 text-left hidden sm:table-cell">
-              Category
-            </th>
-            <th className="px-4 py-2 text-left hidden sm:table-cell">Status</th>
+            <th className="px-4 py-2 text-left">Classification</th>
+            <th className="px-4 py-2 text-left">Category</th>
+            <th className="px-4 py-2 text-left">Status</th>
             <th className="px-4 py-2 text-left">Action</th>
           </tr>
         </thead>
-
         <tbody>
           {items.map((item, index) => (
-            <tr
-              key={item.id}
-              className="border-t hover:bg-gray-50 flex flex-col sm:table-row mb-2 sm:mb-0"
-            >
+            <tr key={item.id} className="border-t hover:bg-gray-50">
               <td className="px-4 py-2">
                 {(currentPage - 1) * itemsPerPage + index + 1}
               </td>
-              <td className="px-4 py-2 font-medium">{item.name}</td>
-              <td className="px-4 py-2 hidden sm:table-cell">
-                {item.classification}
-              </td>
-              <td className="px-4 py-2 hidden sm:table-cell">
-                {item.category}
-              </td>
-              <td className="px-4 py-2 hidden sm:table-cell">{item.status}</td>
+              <td className="px-4 py-2">{item.name}</td>
+              <td className="px-4 py-2">{item.classification}</td>
+              <td className="px-4 py-2">{item.category}</td>
+              <td className="px-4 py-2">{item.status}</td>
               <td className="px-4 py-2">
                 <ActionsButton
                   onView={() => alert(`View ${item.name}`)}
