9b37021219ab2e06485d6ed1148ccfe9ff36af4b
Michael John G. Margate <101804266+Miketzy@users.noreply.github.com>
2026-01-05 16:20:26 +0800

    updated

--- admin-contributor/src/admin-components/createQuest/Create.jsx
+++ admin-contributor/src/admin-components/createQuest/Create.jsx
@@ -55,6 +55,19 @@ function Create() {
             </span>
           </div>
         </div>
+
+        {/* Points Input */}
+        <div className="flex flex-col gap-2">
+          <label className="text-sm font-medium text-gray-700">
+            Points of this answer
+          </label>
+          <input
+            type="number"
+            min="0"
+            placeholder="Enter points"
+            className="w-full border border-gray-300 rounded-lg px-4 py-2 focus:outline-none focus:ring-2 focus:ring-green-500"
+          />
+        </div>
       </div>
     </div>
   );
