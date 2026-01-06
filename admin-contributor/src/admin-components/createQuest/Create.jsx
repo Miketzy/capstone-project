049e19c779879bfefe6168adab9fab2cb1c4744f
Michael John G. Margate <101804266+Miketzy@users.noreply.github.com>
2026-01-06 11:50:42 +0800

    updated

--- admin-contributor/src/admin-components/createQuest/Create.jsx
+++ admin-contributor/src/admin-components/createQuest/Create.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import QuestionInput from "./QuestionInput";
-import MultipleChoiceOptions from "./multiple-choice/MultipleOptionRow";
+import MultipleOptionRow from "./multiple-choice/MultipleOptionRow";
 import Essay from "./essay/Essay";
 import MatchingType from "./matching-type/MatchingType";
 import TrueOrFalse from "./true-or-false/TrueOrFalse";
@@ -9,13 +9,13 @@ function Create() {
   const [selectedType, setSelectedType] = useState(""); // start empty
 
   return (
-    <div className="w-full ">
+    <div className="w-full min-h-screen bg-gray-100 p-4">
       <div className="w-full bg-white rounded-xl shadow-md p-6 flex flex-col gap-4">
         {/* QuestionInput with onChange */}
         <QuestionInput onSelectType={setSelectedType} />
 
         {/* Conditional Rendering: only show if type is selected */}
-        {selectedType === "multiple" && <MultipleChoiceOptions />}
+        {selectedType === "multiple" && <MultipleOptionRow />}
         {selectedType === "matching" && <MatchingType />}
         {selectedType === "essay" && <Essay />}
         {selectedType === "trueorfalse" && <TrueOrFalse />}
