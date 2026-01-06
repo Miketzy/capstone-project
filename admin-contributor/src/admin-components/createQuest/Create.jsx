3a57acfe8dd35045b48cd8beb4882f02219eac47
Michael John G. Margate <101804266+Miketzy@users.noreply.github.com>
2026-01-06 11:21:21 +0800

    updated

--- admin-contributor/src/admin-components/createQuest/Create.jsx
+++ admin-contributor/src/admin-components/createQuest/Create.jsx
@@ -3,7 +3,7 @@ import MultipleChoiceOptions from "./multiple-choice/MultipleOptionRow";
 
 function Create() {
   return (
-    <div className="w-full min-h-screen ">
+    <div className="w-full ">
       <div className="w-full bg-white rounded-xl shadow-md p-6 flex flex-col gap-4">
         <QuestionInput />
         <MultipleChoiceOptions />
