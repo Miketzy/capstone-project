d192e9d442b926ecbdcf994f640c1327cf300f5f
Michael John G. Margate <101804266+Miketzy@users.noreply.github.com>
2026-01-06 13:26:11 +0800

    updateed

--- admin-contributor/src/admin-components/createQuest/true-or-false/TrueOrFalse.jsx
+++ admin-contributor/src/admin-components/createQuest/true-or-false/TrueOrFalse.jsx
@@ -68,13 +68,14 @@ function TrueOrFalse({
         </div>
       </div>
 
-      {/* Submit */}
-      <button
-        type="submit"
-        className="w-fit bg-blue-600 text-white px-6 py-2 rounded-lg hover:bg-blue-700 transition"
-      >
-        Create Question
-      </button>
+      <div className="flex justify-center">
+        <button
+          type="submit"
+          className="bg-blue-600 text-white px-6 py-2 rounded-lg hover:bg-blue-700 transition"
+        >
+          Create Question
+        </button>
+      </div>
     </form>
   );
 }
