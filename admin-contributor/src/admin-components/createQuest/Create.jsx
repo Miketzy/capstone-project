d972b7f983e83b91e22d576218f8da7238f4b640
Michael John G. Margate <101804266+Miketzy@users.noreply.github.com>
2026-01-06 11:07:41 +0800

    updated

--- admin-contributor/src/admin-components/createQuest/Create.jsx
+++ admin-contributor/src/admin-components/createQuest/Create.jsx
@@ -2,7 +2,7 @@ import QuestionInput from "./QuestionInput";
 
 function Create() {
   return (
-    <div className="w-full min-h-screen bg-gray-100 p-4">
+    <div className="w-full min-h-screen ">
       <div className="w-full bg-white rounded-xl shadow-md p-6">
         <QuestionInput />
       </div>
