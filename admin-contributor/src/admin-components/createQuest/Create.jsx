3ce0b36fb8f1d16d70a71fab12963015f84efce3
Michael John G. Margate <101804266+Miketzy@users.noreply.github.com>
2026-01-06 11:11:14 +0800

    updated

--- admin-contributor/src/admin-components/createQuest/Create.jsx
+++ admin-contributor/src/admin-components/createQuest/Create.jsx
@@ -1,10 +1,12 @@
 import QuestionInput from "./QuestionInput";
+import MultipleChoiceOptions from "./multiple-choice/MultipleChoiceOptions";
 
 function Create() {
   return (
     <div className="w-full min-h-screen ">
       <div className="w-full bg-white rounded-xl shadow-md p-6">
         <QuestionInput />
+        <MultipleChoiceOptions />
       </div>
     </div>
   );
