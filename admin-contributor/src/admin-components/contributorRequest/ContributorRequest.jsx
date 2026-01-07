c9d1b5638055b4ede34d90462759ed87e27f24f3
Michael John G. Margate <101804266+Miketzy@users.noreply.github.com>
2026-01-07 21:45:51 +0800

    updated

--- admin-contributor/src/admin-components/contributorRequest/ContributorRequest.jsx
+++ admin-contributor/src/admin-components/contributorRequest/ContributorRequest.jsx
@@ -38,7 +38,7 @@ function ContributorRequest() {
   );
 
   return (
-    <div className="w-full bg-white p-4 sm:p-6 rounded-xl shadow-md border border-gray-200 overflow-x-auto">
+    <div className="w-full bg-white p-4 sm:p-6 rounded-xl shadow-md border border-gray-200 ">
       <div className="flex flex-col gap-4">
         {/* Search */}
         <RequesttSearchBar search={search} setSearch={setSearch} />
