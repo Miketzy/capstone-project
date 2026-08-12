e417b4ab85a4a80d2683994d5031bc98d4fac645
Michael <michaelmargate2@gmail.com>
2026-08-12 16:23:51 +0800

    updated

--- admin-contributor/src/admin-components/dashboard/Dashboard.jsx
+++ admin-contributor/src/admin-components/dashboard/Dashboard.jsx
@@ -12,6 +12,26 @@ function Dashboard() {
     <div className="flex flex-col gap-5">
       {/* Buttons */}
       
+
+      {/* Cards */}
+      <div>
+        {activeTab === "vertebrates" && <VertebratesCard />}
+        {activeTab === "invertebrates" && <InvertebratesCard />}
+      </div>
+
+      <div className="flex flex-col lg:flex-row gap-5">
+        <div className="flex flex-col gap-5">
+          <DashboardStat />
+        </div>
+
+        <div className="flex-1 p-6 bg-white rounded-xl shadow-md border border-gray-200">
+          <AreaChartCard />
+        </div>
+      </div>
+
+      {/**In the Contributor Table, you can see whether a contributor is active or not. */}
+
+      <ContributorTable />
     </div>
   );
 }
